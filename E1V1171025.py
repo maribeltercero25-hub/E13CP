ef8819946a2bd17bfcae252576bfd39e9062493f
maribeltercero25-hub <maribeltercero25@gmail.com>
2025-12-03 17:47:26 +0100

    Add files via upload

---
+++ E1V1171025.py
@@ -0,0 +1,11 @@
+#importar las bibliotecas
+import tkinter as tk
+#configurar la ventana 
+ventana = tk.Tk()
+ventana.title("adicciones a los videojuegos")#agregamos titulo
+ventana.geometry("600x400")#agregamos medidas a la ventana
+ventana.config(bg="#FAF8E6")#agregamos color a la ventana
+
+
+#mandar a traer mi ventana
+ventana.mainloop()
